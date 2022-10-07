3098a39925f43750d6a4198a36d94f72f92d49e4
pavanyelisetti <97426141+pavanyelisetti@users.noreply.github.com>
2022-10-07 11:50:17 +0530

    import java.util.Scanner; class pattern{     public static void main(String[] args)     {         Scanner sc =new Scanner(System.in);         int n = sc.nextInt();         if(n<=2)         {             System.out.println("The pattern is not possible");         }         else         {         for(int i=1;i<=n;i++)         {             for(int j=1;j<=i;j++)             {                 System.out.print("*");             }             System.out.println();         }         for(int i=n-1;i>=1;i--)         {             for(int j=1;j<=i;j++)             {                 System.out.print("*");             }             System.out.println();         }         }     } }

---
+++ Half_diamond_star_pattern.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class pattern{
+    public static void main(String[] args)
+    {
+        Scanner sc =new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n<=2)
+        {
+            System.out.println("The pattern is not possible");
+        }
+        else
+        {
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n-1;i>=1;i--)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        }
+    }
+}
