006404c99601da34f3fb2a72cf8fc372bce81052
pavanyelisetti <97426141+pavanyelisetti@users.noreply.github.com>
2022-10-10 10:03:23 +0530

    import java.util.*; class prim{     public static int prime(int x)     {         int c=0;         for(int i=1;i<=x;i++)         {             if(x%i==0)             {                 c+=1;             }         }         if(c==2)         {             return 1;         }         return 0;     }     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int n=sc.nextInt();         int a[]= new int [100];         int c=0;         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         int k=sc.nextInt();         for(int i=0;i<n;i++)         {             if(prime(a[i])==1)             {                 if(a[i]<=k)                 c+=1;             }         }         System.out.println(c);     } }

---
+++ primes_less_than_k.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class prim{
+    public static int prime(int x)
+    {
+        int c=0;
+        for(int i=1;i<=x;i++)
+        {
+            if(x%i==0)
+            {
+                c+=1;
+            }
+        }
+        if(c==2)
+        {
+            return 1;
+        }
+        return 0;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]= new int [100];
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int k=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            if(prime(a[i])==1)
+            {
+                if(a[i]<=k)
+                c+=1;
+            }
+        }
+        System.out.println(c);
+    }
+}
