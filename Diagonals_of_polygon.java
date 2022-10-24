e62c92cf161fc529332b7641f55d1991283a0f77
pavanyelisetti <97426141+pavanyelisetti@users.noreply.github.com>
2022-10-24 16:14:14 +0530

    import java.util.Scanner; class share {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a;         a=sc.nextInt();         System.out.println((a*(a-3))/2);     } }

---
+++ Diagonals_of_polygon.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class share
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a;
+        a=sc.nextInt();
+        System.out.println((a*(a-3))/2);
+    }
+}
