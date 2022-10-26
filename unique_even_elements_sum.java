053de87a3b9d7d2ca027d235f48b7691c3ba2165
pavanyelisetti <97426141+pavanyelisetti@users.noreply.github.com>
2022-10-26 10:31:52 +0530

    import java.util.Scanner; class sum {     public static void main(String args[])     {         Scanner sc =new Scanner(System.in);         int n=sc.nextInt();         int a[]=new int[100];         int c=0,sum=0;         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             c=0;             for(int j=0;j<n;j++)             {                 if(i!=j)                 {                 if(a[i]==a[j])                 {                     a[j]=-1;                 }                 }             }
            }
            for(int i=0;i<n;i++)
            {
                if(a[i]%2==0)
                {
                    sum+=a[i];
                }
            }
            System.out.println(sum);
        }
        
    }

---
+++ unique_even_elements_sum.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+class sum
+{
+    public static void main(String args[])
+    {
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[100];
+        int c=0,sum=0;
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            c=0;
+            for(int j=0;j<n;j++)
+            {
+                if(i!=j)
+                {
+                if(a[i]==a[j])
+                {
+                    a[j]=-1;
+                }
+                }
+            }
+     
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]%2==0)
+            {
+                sum+=a[i];
+            }
+        }
+        System.out.println(sum);
+    }
+    
+}
