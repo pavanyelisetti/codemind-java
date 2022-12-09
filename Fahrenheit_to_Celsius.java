a6b35ca03944c6cdde58ebe8f4f1eb6793702d65
pavanyelisetti <97426141+pavanyelisetti@users.noreply.github.com>
2022-12-09 22:24:26 +0530

    import java.util.Scanner; class ind {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int n=sc.nextInt();         System.out.format("%.2f",(5*1.0/9)*(n-32));     } }

---
+++ Fahrenheit_to_Celsius.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class ind
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.format("%.2f",(5*1.0/9)*(n-32));
+    }
+}
