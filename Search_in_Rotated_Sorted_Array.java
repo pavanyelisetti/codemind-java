00d0cf69da94645cf0a0a0b4a73e80e3df862e72
pavanyelisetti <97426141+pavanyelisetti@users.noreply.github.com>
2022-12-12 15:41:40 +0530

    import java.util.*; class pro {     public static void main(String[] args)     {         Scanner sc =new Scanner(System.in);         int n=sc.nextInt();         int a[]=new int[100];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         int x=sc.nextInt(),f=0;         for(int i=0;i<n;i++)         {             if(a[i]==x)             {                 System.out.println(i);                 f=1;                 break;             }         }         if(f==0)         {             System.out.println("-1");         }
        }
    }

---
+++ Search_in_Rotated_Sorted_Array.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class pro
+{
+    public static void main(String[] args)
+    {
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[100];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int x=sc.nextInt(),f=0;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]==x)
+            {
+                System.out.println(i);
+                f=1;
+                break;
+            }
+        }
+        if(f==0)
+        {
+            System.out.println("-1");
+        }
+
+    }
+}
